a1365aaaf8d54375be49b672423d3b58126688ad
Edward Tomasz Napierała <trasz@FreeBSD.org>
2011-12-21 14:28:00 +0100

    Extract channel name.

--- irclog2sqlite.py
+++ irclog2sqlite.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+# create table irclogs (channel text, time timestamp, seq integer, line text);
+
 import calendar
+import re
 import sqlite3
 import sys
 import time
 
 if len(sys.argv) != 3:
-	print "usage: " + sys.argv[0] + " file-name db-name"
+	print "usage: " + sys.argv[0] + " irclog-path db-path"
 	sys.exit(1)
 
+# We cannot use codecs.open() here instead, as it seems to split lines on invalid UTF-8 sequences.
 f = open(sys.argv[1], 'r')
 db = sqlite3.connect(sys.argv[2])
 cursor = db.cursor()
+prev_msgtime = 0
+prev_msgchan = ""
 
 while True:
 	l = f.readline()
@@ -23,11 +29,10 @@ while True:
 		continue;
 
 	if l.startswith("--- Log opened"):
-		# We are only interested in date here.
+		# We are only interested in date here; subsequent calculations depend on hour and minute being 0.
 		s = l.split()
 		timestr = s[4] + " " + s[5] + " " + s[7]
 		opened_at = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
-		prev_msgtime = 0
 		# The 'opened_at' variable contains log open time, in seconds since epoch.
 		continue
 
@@ -35,12 +40,20 @@ while True:
 		s = l.split()
 		timestr = s[4] + " " + s[5] + " " + s[6]
 		opened_at = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
-		prev_msgtime = 0
 		continue
 
 	if l.startswith("--- Log closed"):
 		del opened_at
 		continue
+
+	# 12:21 -!- jot [jceel@apcoh.org] has joined #gusta-muzyczne-milosnikow-bsd-i-open-source
+	if re.search(r"^..... -!- .* has joined", l):
+		(msgchan, dummy) = re.subn(r"^..... -!- .* has joined ", r"", l)
+		if prev_msgchan and msgchan != prev_msgchan:
+			print "ERROR: channel changed; was ", prev_msgchan, "; changed to ", msgchan
+			print "at line '", l, "'"
+			exit(10);
+		prev_msgchan = msgchan
 
 	timestr = l.split()[0]
 	t = time.strptime(timestr, "%H:%M")
@@ -57,7 +70,7 @@ while True:
 	prev_msgtime = msgtime
 	msgstr = " ".join(l.split()[1:])
 	#print time.strftime("%Y-%m-%d %H:%M", time.gmtime(msgtime)), msgseq, msgstr
-	cursor.execute('insert into irclogs values (?, ?, ?)', [time.strftime("%Y-%m-%d %H:%M", time.gmtime(msgtime)), msgseq, unicode(msgstr, 'utf8', errors = 'replace')]);
+	cursor.execute('insert into irclogs (channel, time, seq, line) values (?, ?, ?, ?)', [msgchan, time.strftime("%Y-%m-%d %H:%M", time.gmtime(msgtime)), msgseq, unicode(msgstr, 'utf8', errors = 'replace')]);
 
 db.commit()
 
