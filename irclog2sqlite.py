842298387b99b850381317ba70ad266065ebc92e
Edward Tomasz Napierała <trasz@FreeBSD.org>
2011-12-22 15:11:35 +0100

    Keep open and close times for chunks.

--- irclog2sqlite.py
+++ irclog2sqlite.py
@@ -13,9 +13,14 @@ import time
 def time2str(s):
 	return time.strftime("%Y-%m-%d %H:%M", time.gmtime(s))
 
-def create_chunk(ch):
-	cursor.execute('insert into chunks (channel, imported_from, imported_at, imported_by) values (?, ?, ?, ?)', [ch, filename, imported_at, imported_by])
+def log_opened(ch, opened_at):
+	cursor.execute('insert into chunks (channel, opened_at, imported_from, imported_at, imported_by) values (?, ?, ?, ?, ?)', [ch, time2str(opened_at), filename, imported_at, imported_by])
 	return cursor.lastrowid
+
+def log_closed(chunk_id, closed_at):
+	cursor.execute('update chunks set closed_at = ? where id = ?', [time2str(closed_at), chunk_id])
+	if cursor.rowcount != 1:
+		raise Exception("weird number of rows affected: was " + str(cursor.rowcount) + ", should be 1")
 
 if len(sys.argv) != 3:
 	print "usage: " + sys.argv[0] + " irclog-path db-path"
@@ -33,7 +38,7 @@ imported_by = os.getlogin() + "@" + os.uname()[1]
 imported_at = time.ctime()
 
 try:
-	cursor.execute('create table chunks (id integer primary key autoincrement, channel text not null, imported_from text not null, imported_at timestamp not null, imported_by text not null)')
+	cursor.execute('create table chunks (id integer primary key autoincrement, channel text not null, opened_at timestamp not null, closed_at timestamp, imported_from text not null, imported_at timestamp not null, imported_by text not null, unique (channel, opened_at, closed_at))')
 	cursor.execute('create table entries (chunk_id integer, time timestamp not null, seq integer not null, line text not null, foreign key(chunk_id) references chunks(id))')
 except:
 	pass
@@ -47,25 +52,32 @@ while True:
 
 	l = l.rstrip()
 	if not l: # Empty line.
-		continue;
+		continue
 
 	try:
 		if l.startswith("--- Log opened"):
-			# We are only interested in date here; subsequent calculations depend on hour and minute being 0.
+			timestr = " ".join(l.split()[3:])
+			opened_at = calendar.timegm(time.strptime(timestr))
+
+			# Subsequent calculations depend on hour and minute being 0.
 			s = l.split()
 			timestr = s[4] + " " + s[5] + " " + s[7]
-			# The 'opened_at' variable contains log open time, in seconds since epoch.
-			opened_at = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
+			opened_date = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
 			continue
 
 		if l.startswith("--- Day changed"):
 			s = l.split()
 			timestr = s[4] + " " + s[5] + " " + s[6]
-			opened_at = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
+			opened_date = calendar.timegm(time.strptime(timestr, "%b %d %Y"))
 			continue
 
 		if l.startswith("--- Log closed"):
+			timestr = " ".join(l.split()[3:])
+			closed_at = calendar.timegm(time.strptime(timestr))
+			log_closed(chunk_id, closed_at)
 			del opened_at
+			del opened_date
+			del closed_at
 			del chunk_id
 			prev_channel = ""
 			continue
@@ -74,15 +86,15 @@ while True:
 			(channel, dummy) = re.subn(r"^..... -!- .* has joined ", r"", l)
 			channel = channel.lower()
 			if not prev_channel:
-				chunk_id = create_chunk(channel)
+				chunk_id = log_opened(channel, opened_at)
+				prev_channel = channel
 			elif channel != prev_channel:
 				raise Exception("channel changed; was " + prev_channel + "; changed to " + channel)
-			prev_channel = channel
 
 		timestr = l.split()[0]
 		t = time.strptime(timestr, "%H:%M")
 
-		entry_time = opened_at + (t.tm_hour * 60 + t.tm_min) * 60 
+		entry_time = opened_date + (t.tm_hour * 60 + t.tm_min) * 60 
 		if entry_time < prev_entry_time:
 			raise Exception("time going backwards; previously " + time2str(prev_entry_time) + ", now " + time2str(entry_time))
 		if entry_time == prev_entry_time:
